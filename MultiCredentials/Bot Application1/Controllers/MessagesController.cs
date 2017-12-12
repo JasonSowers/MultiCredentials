c7a85b962b439057bb076074f8e6715970af9f6f
Jason Sowers <v-jassow@microsoft.com>
2017-12-11 17:13:48 -0800

    initial commit

---
+++ MultiCredentials/Bot Application1/Controllers/MessagesController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Autofac;
+using Bot_Application1;
+using Bot_Application1.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+
+
+namespace Microsoft.Bot.Sample.SimpleMultiCredentialBot
+{
+
+    /// <summary>
+    /// A sample ICredentialProvider that is configured by multiple MicrosoftAppIds and MicrosoftAppPasswords
+    /// </summary>
+    public class MultiCredentialProvider : ICredentialProvider
+    {
+        public Dictionary<string, string> Credentials = new Dictionary<string, string>
+        {
+            {"84cfb056-522b-48a3-be22-d18451b7f80f", "osiegUN68fxGDSXE810=(?%"}
+        };
+
+        public Task<bool> IsValidAppIdAsync(string appId)
+        {
+            return Task.FromResult(this.Credentials.ContainsKey(appId));
+        }
+
+        public Task<string> GetAppPasswordAsync(string appId)
+        {
+            return Task.FromResult(this.Credentials.ContainsKey(appId) ? this.Credentials[appId] : null);
+        }
+
+        public Task<bool> IsAuthenticationDisabledAsync()
+        {
+            return Task.FromResult(!this.Credentials.Any());
+        }
+    }
+
+    /// Use the MultiCredentialProvider as credential provider for BotAuthentication
+    [BotAuthentication(CredentialProviderType = typeof(MultiCredentialProvider))]
+    public class MessagesController : ApiController
+    {
+
+
+        static MessagesController()
+        {
+            // Update the container to use the right MicorosftAppCredentials based on
+            // Identity set by BotAuthentication
+            var builder = new ContainerBuilder();
+
+            builder.Register(c => Class1.GetCredentialsFromClaims(((ClaimsIdentity)HttpContext.Current.User.Identity)))
+                .AsSelf()
+                .InstancePerLifetimeScope();
+            builder.Update(Conversation.Container);
+        }
+
+        /// <summary>
+        /// POST: api/Messages
+        /// receive a message from a user and send replies
+        /// </summary>
+        /// <param name="activity"></param>
+        [ResponseType(typeof(void))]
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            if (activity != null)
+            {
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, () => new RootDialog());
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                        IConversationUpdateActivity update = activity;
+                        // resolve the connector client from the container to make sure that it is 
+                        // instantiated with the right MicrosoftAppCredentials
+                        using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, activity))
+                        {
+                            var client = scope.Resolve<IConnectorClient>();
+                            if (update.MembersAdded.Any())
+                            {
+                                var reply = activity.CreateReply();
+                                foreach (var newMember in update.MembersAdded)
+                                {
+                                    if (newMember.Id != activity.Recipient.Id)
+                                    {
+                                        reply.Text = $"Welcome {newMember.Name}!";
+                                        await client.Conversations.ReplyToActivityAsync(reply);
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    case ActivityTypes.Ping:
+                    default:
+                        Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                        break;
+                }
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+    }
+}
